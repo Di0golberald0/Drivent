7a12dd5a59493aba4ba7cbd7e40caf7dbc66bf5d
Diogo Lima Beraldo <diogolberaldo@gmail.com>
2023-07-17 17:27:34 -0300

    fix: tests should now run errors 402 & 401

--- src/controllers/hotel-controller.ts
+++ src/controllers/hotel-controller.ts
@@ -1,23 +1,20 @@
-import { Response } from "express";
+import { Response, NextFunction } from "express";
 import { AuthenticatedRequest } from "@/middlewares";
 import hotelService from "@/services/hotels-service";
 import httpStatus from "http-status";
 
-export async function getHotels(req: AuthenticatedRequest, res: Response) {
+export async function getHotels(req: AuthenticatedRequest, res: Response, next: NextFunction) {
   const { userId } = req;
 
   try {
     const hotels = await hotelService.getHotels(Number(userId));
     return res.status(httpStatus.OK).send(hotels);
   } catch (error) {
-    if (error.name === "NotFoundError") {
-      return res.sendStatus(httpStatus.NOT_FOUND);
-    }
-    return res.sendStatus(httpStatus.PAYMENT_REQUIRED);
+    next(error);
   }
 };
 
-export async function getHotelsWithRooms(req: AuthenticatedRequest, res: Response) {
+export async function getHotelsWithRooms(req: AuthenticatedRequest, res: Response, next: NextFunction) {
   const { userId } = req;
   const { hotelId } = req.params;
 
@@ -26,12 +23,6 @@ export async function getHotelsWithRooms(req: AuthenticatedRequest, res: Respons
 
     return res.status(httpStatus.OK).send(hotels);
   } catch (error) {
-    if (error.name === "NotFoundError") {
-      return res.sendStatus(httpStatus.NOT_FOUND);
-    }
-    if (error.name === "CannotListHotelsError") {
-      return res.sendStatus(httpStatus.PAYMENT_REQUIRED);
-    }
-    return res.sendStatus(httpStatus.BAD_REQUEST);
+    next(error);
   }
 };
--- src/middlewares/error-handling-middleware.ts
+++ src/middlewares/error-handling-middleware.ts
@@ -38,10 +38,28 @@ export function handleApplicationErrors(
     });
   }
 
+  if (err.name === 'CannotListHotelsError') {
+    return res.status(httpStatus.NOT_FOUND).send({
+      message: err.message,
+    });
+  }
+
+  if (err.name === 'BadRequestError') {
+    return res.status(httpStatus.BAD_REQUEST).send({
+      message: err.message,
+    });
+  }
+
+  if (err.name === 'ForBiddenError') {
+    return res.status(httpStatus.FORBIDDEN).send({
+      message: err.message,
+    });
+  }
+
   /* eslint-disable-next-line no-console */
   console.error(err.name);
   res.status(httpStatus.INTERNAL_SERVER_ERROR).send({
     error: 'InternalServerError',
     message: 'Internal Server Error',
   });
-}
+};
